bbd2aede6da0aa2ff653459d50611ff0f2eb3850
thomas-huguenin <thomas.huguenin@gmail.com>
2016-12-08 00:04:53 +0100

    Footer and page width

--- Templates Joomla/Reborn/layout/lyt_footer.php
+++ Templates Joomla/Reborn/layout/lyt_footer.php
@@ -3,5 +3,7 @@
       <h1>ASSIDU</h1>
       <p>Le r&eacute;seau des anciens de l'UTBM, ENIBe, UTCS et IPS&eacute;<br>13 rue Thierry Mieg 90 000 belfort</p>
     </div>
-    <jdoc:include type="modules" name="Footer-Accueil" style="xhtml" />
+    <div>
+      <jdoc:include type="modules" name="Footer-Right" style="xhtml" />
+    </div>
   </div>
